bc56e2c472d943a2461a9f2a8b6348060e63bc11
Mads Hartmann <mads379@gmail.com>
2022-06-10 08:07:11 +0000

    Works with node:16.15.0-alpine

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { exec } = require("child_process");
+
+function sh(command) {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+sh("id -u")
+sh("id -g")
+
+console.log("hello")
